fe31ba95360e39589cfe77a692de1f5bf3d0cb84
antonionastri <nastritoni98@gmail.com>
2024-08-07 20:13:06 +0200

    1.4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,5 +8,5 @@ export const routes: Routes = [
     },
     {
         path:"menu", component:MenuComponent
-    }
+    },
 ];
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, RouterOutlet, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+  activeCategory: string = '';
+  
+  ngOnInit(){
+    this.activeCategory = 'caffe';
+  }
 
+  showCategory(categoryId: string) {
+    this.activeCategory = categoryId;
+  }
 }
